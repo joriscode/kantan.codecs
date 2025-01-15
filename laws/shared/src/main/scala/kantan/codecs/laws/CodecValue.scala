d1fd0da88fc912888a782a1bf95b9dcccdf56dd2
Joris <2750485+joriscode@users.noreply.github.com>
2025-01-15 10:53:30 +0100

    chore: Format (scalafix, scalafmt)

--- laws/shared/src/main/scala/kantan/codecs/laws/CodecValue.scala
+++ laws/shared/src/main/scala/kantan/codecs/laws/CodecValue.scala
@@ -26,23 +26,30 @@ sealed abstract class CodecValue[E, D, T] extends Product with Serializable {
   def tag[TT]: CodecValue[E, D, TT]
 
   def isLegal: Boolean
-  def isIllegal: Boolean = !isLegal
+  def isIllegal: Boolean =
+    !isLegal
 }
 
 object CodecValue {
   final case class LegalValue[E, D, T](encoded: E, decoded: D) extends CodecValue[E, D, T] {
-    override def mapDecoded[DD](f: D => DD) = LegalValue(encoded, f(decoded))
-    override def mapEncoded[EE](f: E => EE) = LegalValue(f(encoded), decoded)
+    override def mapDecoded[DD](f: D => DD) =
+      LegalValue(encoded, f(decoded))
+    override def mapEncoded[EE](f: E => EE) =
+      LegalValue(f(encoded), decoded)
     @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-    override def tag[TT] = this.asInstanceOf[LegalValue[E, D, TT]]
+    override def tag[TT]: CodecValue[E, D, TT] =
+      this.asInstanceOf[LegalValue[E, D, TT]]
     override val isLegal = true
   }
 
   final case class IllegalValue[E, D, T](encoded: E) extends CodecValue[E, D, T] {
-    override def mapDecoded[DD](f: D => DD) = IllegalValue(encoded)
-    override def mapEncoded[EE](f: E => EE) = IllegalValue(f(encoded))
+    override def mapDecoded[DD](f: D => DD) =
+      IllegalValue(encoded)
+    override def mapEncoded[EE](f: E => EE) =
+      IllegalValue(f(encoded))
     @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-    override def tag[TT] = this.asInstanceOf[IllegalValue[E, D, TT]]
+    override def tag[TT]: CodecValue[E, D, TT] =
+      this.asInstanceOf[IllegalValue[E, D, TT]]
     override val isLegal = false
   }
 }
